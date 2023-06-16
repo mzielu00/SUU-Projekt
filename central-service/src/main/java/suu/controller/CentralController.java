6f34f1d47e66eb7d4fcf8b7a6649436af636c75c
Zofia Grodecka <zgrodecka@gmail.com>
2023-06-16 12:13:39 +0200

    Add central controller

---
+++ central-service/src/main/java/suu/controller/CentralController.java
@@ -0,0 +1,26 @@
+package suu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import suu.clients.Edge1Connector;
+
+@RestController
+@RequestMapping("/data")
+public class CentralController {
+    @Autowired
+    private final Edge1Connector edge1Connector;
+
+    public CentralController(Edge1Connector edge1Connector) {
+        this.edge1Connector = edge1Connector;
+    }
+
+    @GetMapping("/traffic")
+    public ResponseEntity<Double> getAverageTrafficCongestion() {
+        Double edge1Traffic = edge1Connector.getTrafficCongestionAverageEdge1();
+
+        return ResponseEntity.ok(edge1Traffic);
+    }
+}
---
+++ central-service/src/main/java/suu/model/AirPollutionStatus.java
@@ -0,0 +1,30 @@
+package suu.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class AirPollutionStatus {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+  private int PM10;
+  private int PM2_5;
+  private int PM1;
+
+  @Override
+  public String toString(){
+    return "PM10:" + PM10 + ",PM2.5:" + PM2_5 + "PM1:" + PM1;
+  }
+}
