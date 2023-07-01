93821d1b1f788ef0c5cdf09437e449b72394d1b1
zofiagrodecka <69384237+zofiagrodecka@users.noreply.github.com>
2023-07-01 15:15:41 +0200

    Add central-service config

--- central-service/src/main/java/suu/clients/Edge1Connector.java
+++ central-service/src/main/java/suu/clients/Edge1Connector.java
@@ -2,9 +2,10 @@ package suu.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import suu.config.Edge1Config;
 import suu.model.AirPollutionStatus;
 
-@FeignClient(name = "edge1", url = "http://localhost:8260")
+@FeignClient(name = "edge1", configuration= Edge1Config.class)
 public interface Edge1Connector {
     @GetMapping("/edge1/srodmiescie/air/average")
     AirPollutionStatus getAirPollutionAverageEdge1();
